21c6e78f733ec3b08ad48a72fa67658785c56661
Sherwin Espela <sherwinjai_espela@yahoo.ca>
2023-11-30 15:58:22 -0800

    Player controls and animation blueprint setup wip 1

---
+++ GearsOfSocom/Source/GearsOfSocom/Public/Characters/GOSPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Copyright 2023 Sherwin Espela. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/GOSBaseCharacter.h"
+#include "GOSPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEARSOFSOCOM_API AGOSPlayerCharacter : public AGOSBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ GearsOfSocom/Source/GearsOfSocom/Public/GameMode/GOSGameMode.h
@@ -0,0 +1,17 @@
+// Copyright 2023 Sherwin Espela. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GOSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEARSOFSOCOM_API AGOSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
