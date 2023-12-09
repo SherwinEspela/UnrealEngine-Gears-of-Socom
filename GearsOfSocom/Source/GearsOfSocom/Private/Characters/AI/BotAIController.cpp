896ef8e9753a4e806d443c71833745f02e367532
Sherwin Espela <sherwinjai_espela@yahoo.ca>
2023-12-09 13:13:48 -0800

    Enemy AI - Behavior Tree and Blackboard setup

---
+++ GearsOfSocom/Source/GearsOfSocom/Private/Characters/AI/BotAIController.cpp
@@ -0,0 +1,35 @@
+// Copyright 2023 Sherwin Espela. All rights reserved.
+
+
+#include "Characters/AI/BotAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void ABotAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (BehaviorTree)
+	{
+		RunBehaviorTree(BehaviorTree);
+
+		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+	}
+}
+
+void ABotAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (LineOfSightTo(PlayerPawn))
+	{
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
+	}
+	else {
+		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+	}
+}
